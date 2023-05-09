4ebfc5e3149353eac55628cd758a8d7bf7cd976b
Ariane Albuquerque <arianeoalbuquerques@gmail.com>
2023-05-09 14:59:20 -0300

    Atualização HTML

---
+++ Exercicios/Ex09_CondicoesCompostas.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){ //Condição composta if e else;
+    console.log('Brasileiro!')
+}else{
+    console.log('Estrangeiro!')
+}
